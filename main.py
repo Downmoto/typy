19bc78206b4a166d3f9f407ed5187473fc52e5bf
Arad Fadaei <aradfadaie@gmail.com>
2022-09-13 12:59:40 -0400

    profile class additions, typing changes

--- main.py
+++ main.py
@@ -1,33 +1,92 @@
 from typing import Union
+
 from pathlib import Path
 from random import choice
-from json import load
+from json import load, dump
 from time import time
+from os.path import exists
 
 from rich import print
+# from rich.pretty import pprint
 from rich import box
 from rich.prompt import Prompt
 from rich.panel import Panel
 from rich.text import Text
 
+DEBUG: bool = True
+
 pathToWords: Path = Path('./words.json')
+
+if DEBUG:
+    pathtoProfile: Path = Path('./profile.json')
+else:
+    pathtoProfile: Path = Path.home()
+
 
 class Profile:
     def __init__(self) -> None:
-        pass
+        self.profile: dict = self.__load_profile()
+    
+    def __check_profile_exists(self) -> bool:
+        return exists(pathtoProfile)
 
-
+    def __load_profile(self) -> dict:
+        mode: str = 'r' if self.__check_profile_exists() else 'w'
+        
+        with open(pathtoProfile, mode) as profile:
+            p: dict =self.__profile_base()
+            
+            if mode == 'r':
+                p = load(profile)
+            else:
+                dump(p, profile)
+                
+            return p
+            
+    def __profile_base(self) -> dict:
+        return {
+            "name": "str",
+            "accuracy_average": 0,
+            "runs": 0,
+            "average_difficulty": 0.00,
+            "encounters": {
+                "top": {
+                    "words": [
+                        {
+                            "word": "0",
+                            "encountered": 0
+                        },
+                        {
+                            "word": "0",
+                            "encountered": 0
+                        },
+                        {
+                            "word": "0",
+                            "encountered": 0
+                        }
+                    ],
+                    "letters": [
+                        {
+                            "letter": "0",
+                            "encountered": 0
+                        }
+                    ]
+                }
+            }
+        }
+        
+        
 class Word:
     def __init__(self, word: str) -> None:
         self.word: str = word
         self.diff: int = len(word)
 
-        self.letters: list[str] = self.__generateLetters()
+        self.letters: list[str] = self.__generate_letters()
 
     def __log(self):
         print(self.word)
 
-    def __generateLetters(self) -> list[str]:
+    def __generate_letters(self) -> list[str]:
         letters: list[str] = []
         for l in self.word:
             letters.append(l)
@@ -84,6 +143,18 @@ def main():
 
     print(speed)
 
+
+def debug():
+    p = Profile()
+    
+    print(p.profile['name'])
+    
+
     
 if __name__ == '__main__':
-    main()
+    if DEBUG:
+        debug()
+    else:
+        main()
+    
+    
